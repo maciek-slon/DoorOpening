eeb31112739226d118e3208bffc345bbac481014
Maciej Stefańczyk <maciek.slon@gmail.com>
2013-11-18 06:53:45 +0100

    VanishingPoint

---
+++ src/Components/VanishingPointsEstimator/VanishingPointsEstimator.cpp
@@ -0,0 +1,91 @@
+/*!
+ * \file
+ * \brief
+ * \author maciej,,,
+ */
+
+#include <memory>
+#include <string>
+
+#include "VanishingPointsEstimator.hpp"
+#include "Common/Logger.hpp"
+
+#include "Types/Line.hpp"
+
+#include <boost/bind.hpp>
+
+
+namespace Processors {
+namespace VanishingPointsEstimator {
+
+VanishingPointsEstimator::VanishingPointsEstimator(const std::string & name) :
+		Base::Component(name)  {
+
+}
+
+VanishingPointsEstimator::~VanishingPointsEstimator() {
+}
+
+void VanishingPointsEstimator::prepareInterface() {
+	// Register data streams, events and event handlers HERE!
+registerStream("in_lines", &in_lines);
+registerStream("out_lines", &out_lines);
+registerStream("out_points", &out_points);
+	// Register handlers
+	h_onNewLines.setup(boost::bind(&VanishingPointsEstimator::onNewLines, this));
+	registerHandler("onNewLines", &h_onNewLines);
+	addDependency("onNewLines", &in_lines);
+
+}
+
+bool VanishingPointsEstimator::onInit() {
+
+	return true;
+}
+
+bool VanishingPointsEstimator::onFinish() {
+	return true;
+}
+
+bool VanishingPointsEstimator::onStop() {
+	return true;
+}
+
+bool VanishingPointsEstimator::onStart() {
+	return true;
+}
+
+void VanishingPointsEstimator::onNewLines() {
+	Types::DrawableContainer lines = in_lines.read();
+	Types::Line * line;
+
+	std::vector<Line*> lines_v, lines_h;
+
+	for (int i = 0; i < lines.size(); ++i) {
+		line = static_cast<Types::Line*>(lines.get(i));
+		if (abs(line->getAngle()) < M_PI/6) {
+			line->setCol(cv::Scalar(0, 0, 255));
+			lines_h.push_back(line);
+		} else
+		if (abs(line->getAngle()) > 2*M_PI/6) {
+			line->setCol(cv::Scalar(0, 255, 0));
+			lines_v.push_back(line);
+		} else {
+			line->setCol(cv::Scalar(255, 0, 0));
+		}
+	}
+
+	std::vector<cv::Point2f> points_v, points_h;
+
+	for (int i = 0; i < 20; ++i) {
+		std::random_shuffle(lines_v.begin(), lines_v.end());
+
+	}
+
+	out_lines.write(lines);
+}
+
+
+
+} //: namespace VanishingPointsEstimator
+} //: namespace Processors
---
+++ src/Components/VanishingPointsEstimator/VanishingPointsEstimator.hpp
@@ -0,0 +1,96 @@
+/*!
+ * \file
+ * \brief 
+ * \author maciej,,,
+ */
+
+#ifndef VANISHINGPOINTSESTIMATOR_HPP_
+#define VANISHINGPOINTSESTIMATOR_HPP_
+
+#include "Component_Aux.hpp"
+#include "Component.hpp"
+#include "DataStream.hpp"
+#include "Property.hpp"
+#include "EventHandler2.hpp"
+
+#include <opencv2/opencv.hpp>
+
+#include "Types/DrawableContainer.hpp"
+
+namespace Processors {
+namespace VanishingPointsEstimator {
+
+/*!
+ * \class VanishingPointsEstimator
+ * \brief VanishingPointsEstimator processor class.
+ *
+ * VanishingPointsEstimator processor.
+ */
+class VanishingPointsEstimator: public Base::Component {
+public:
+	/*!
+	 * Constructor.
+	 */
+	VanishingPointsEstimator(const std::string & name = "VanishingPointsEstimator");
+
+	/*!
+	 * Destructor
+	 */
+	virtual ~VanishingPointsEstimator();
+
+	/*!
+	 * Prepare components interface (register streams and handlers).
+	 * At this point, all properties are already initialized and loaded to 
+	 * values set in config file.
+	 */
+	void prepareInterface();
+
+protected:
+
+	/*!
+	 * Connects source to given device.
+	 */
+	bool onInit();
+
+	/*!
+	 * Disconnect source from device, closes streams, etc.
+	 */
+	bool onFinish();
+
+	/*!
+	 * Start component
+	 */
+	bool onStart();
+
+	/*!
+	 * Stop component
+	 */
+	bool onStop();
+
+
+// Input data streams
+
+		Base::DataStreamIn<Types::DrawableContainer> in_lines;
+
+// Output data streams
+
+		Base::DataStreamOut<Types::DrawableContainer> out_lines;
+		Base::DataStreamOut<Types::DrawableContainer> out_points;
+	// Handlers
+	Base::EventHandler2 h_onNewLines;
+
+	
+	// Handlers
+	void onNewLines();
+
+};
+
+} //: namespace VanishingPointsEstimator
+} //: namespace Processors
+
+/*
+ * Register processor component.
+ */
+REGISTER_COMPONENT("VanishingPointsEstimator", Processors::VanishingPointsEstimator::VanishingPointsEstimator)
+
+#endif /* VANISHINGPOINTSESTIMATOR_HPP_ */
